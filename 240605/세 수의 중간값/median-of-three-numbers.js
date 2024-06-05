ba799ecf345479bdebd1f55d9ab64789cda5f1c4
subinred <103013841+subinred@users.noreply.github.com>
2024-06-05 22:38:01 +0900

    Title: 세 수의 중간값, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240605/세 수의 중간값/median-of-three-numbers.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+if (b > a && b < c) {
+    console.log(1);
+} else {
+    console.log(0);
+}
